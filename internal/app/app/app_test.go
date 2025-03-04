ead43f042770f6bf6d0992191be1a6119edc36ed
sinfirst <nikitakoz2006@gmail.com>
2025-03-04 15:47:20 +0300

    Инкримент 2

--- internal/app/app/app_test.go
+++ internal/app/app/app_test.go
@@ -1,55 +1,97 @@
 package app
 
-// import (
-// 	"bytes"
-// 	"example/internal/app/router"
-// 	"example/internal/app/storage"
-// 	"fmt"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"testing"
-// )
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-// func TestTestHandler(t *testing.T) {
-// 	tests := []struct {
-// 		name             string
-// 		URL              string
-// 		wantedPostStatus int
-// 		wantedGetStatus  int
-// 	}{
-// 		{
-// 			name:             "simple test #1",
-// 			URL:              "http://example.ru/",
-// 			wantedPostStatus: http.StatusCreated,
-// 			wantedGetStatus:  http.StatusTemporaryRedirect,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			stg := storage.NewStorage()
-// 			a := App{storage: stg}
-// 			rout := router.NewRouter(&a)
-// 			req := httptest.NewRequest("POST", "/", bytes.NewReader([]byte(tt.URL)))
-// 			rr := httptest.NewRecorder()
-// 			a.PostHandler(rr, req)
-// 			if rr.Code != tt.wantedPostStatus {
-// 				t.Errorf("expected status %d, got %d", http.StatusCreated, rr.Code)
-// 			}
-// 			shortURL := rr.Body.String()[22:]
-// 			fmt.Println("test 1 ", shortURL)
-// 			req = httptest.NewRequest("GET", "/"+shortURL, nil)
-// 			fmt.Println(req.URL.Path)
+	"github.com/sinfirst/URL-Cutter/internal/app/config"
+	"github.com/sinfirst/URL-Cutter/internal/app/storage"
+)
 
-// 			rr = httptest.NewRecorder()
-// 			fmt.Println("test 2 ", a.storage)
-// 			a.GetHandler(rr, req)
-// 			if rr.Code != tt.wantedGetStatus {
-// 				t.Errorf("expected status %d, got %d", http.StatusTemporaryRedirect, rr.Code)
-// 			}
-// 			if loc := rr.Header().Get("Location"); loc != tt.URL {
-// 				t.Errorf("expected Location header %s, got %s", tt.URL, loc)
-// 			}
-// 		})
+func TestGet(t *testing.T) {
+	stg := storage.NewStorage()
+	cfg := config.NewConfig()
+	a := NewApp(stg, cfg)
 
-// 	}
-// }
+	tests := []struct {
+		name           string
+		method         string
+		shortURL       string
+		expectedCode   int
+		expectedHeader string
+	}{
+		{
+			name:           "Invalid short URL",
+			method:         http.MethodGet,
+			shortURL:       "asdoif",
+			expectedCode:   http.StatusBadRequest,
+			expectedHeader: "",
+		},
+		{
+			name:           "Invalid method",
+			method:         http.MethodPost,
+			shortURL:       "asajkdashgkj",
+			expectedCode:   http.StatusBadRequest,
+			expectedHeader: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(tt.method, "/"+tt.shortURL, nil)
+			resRec := httptest.NewRecorder()
+
+			a.PostHandler(resRec, req)
+
+			if resRec.Code != tt.expectedCode {
+				t.Errorf("expected status %d, got %d", tt.expectedCode, resRec.Code)
+			}
+
+			locationHeader := resRec.Header().Get("Location")
+			if locationHeader != tt.expectedHeader {
+				t.Errorf("expected header %v got %v", tt.expectedHeader, locationHeader)
+			}
+		})
+	}
+}
+
+func TestPost(t *testing.T) {
+	stg := storage.NewStorage()
+	cfg := config.NewConfig()
+	a := NewApp(stg, cfg)
+	tests := []struct {
+		name         string
+		method       string
+		url          string
+		expectedCode int
+	}{
+		{
+			name:         "Simple POST request",
+			method:       http.MethodPost,
+			url:          "https://yandex.ru",
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "Invalid request",
+			method:       http.MethodGet,
+			url:          "",
+			expectedCode: http.StatusBadRequest,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var req *http.Request
+			if tt.method == http.MethodPost {
+				req = httptest.NewRequest(tt.method, "http://localhost:8080/", bytes.NewBufferString(tt.url))
+			} else {
+				req = httptest.NewRequest(tt.method, "http://localhost:8080/", nil)
+			}
+			rr := httptest.NewRecorder()
+			a.PostHandler(rr, req)
+			if rr.Code != tt.expectedCode {
+				t.Errorf("expected status %d, got %d", tt.expectedCode, rr.Code)
+			}
+		})
+	}
+}
