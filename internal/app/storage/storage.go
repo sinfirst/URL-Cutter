e765408b33475e8c1377a5cf4494b3fba8adb2ef
sinfirst <nikitakoz2006@gmail.com>
2025-04-17 12:28:11 +0300

    Правки по ревью

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -41,9 +41,7 @@ func NewMapStorage() *MapStorage {
 func NewStorage(conf config.Config, logger zap.SugaredLogger) Storage {
 	if conf.DatabaseDsn != "" {
 		logger.Infow("DB config")
-		if conf.DatabaseDsn != "" {
-			postgresbd.InitMigrations(conf, logger)
-		}
+		postgresbd.InitMigrations(conf, logger)
 		return postgresbd.NewPGDB(conf, logger)
 	}
 	if conf.FilePath != "" {
